667d8dca0970424c0e6b18d34c1c243de907d156
DimaSHMEL <55811573+DimaSHMEL@users.noreply.github.com>
2022-03-01 04:59:06 +0300

    Dandy_bot my vision

--- user_bot.py
+++ user_bot.py
@@ -1,4 +1,75 @@
 ﻿def script(check, x, y):
-    if check("level") == 1:
-        return "right"
+    if check('level') == 1:
+        if check("gold", x, y):
+            return "take"
+        if  check("gold", x , y + 1):
+            return "down"
+        elif check("gold", x  + 1, y):
+            return "right"
+        elif check("gold", x, y - 1):
+            return "up"
+        elif check("gold", x -  1, y):
+            return "left"
+        elif check("wall", x + 1, y) != 1:
+            return "right"
+        elif check("wall", x , y + 1) != 1:
+            return "down"
+    elif check('level') == 2:
+        if check("gold", x, y):
+            return "take"
+        if  check("gold", x , y + 1):
+            return "down"
+        elif check("gold", x  + 1, y):
+            return "right"
+        elif check("gold", x, y - 1):
+            return "up"
+        elif check("gold", x -  1, y):
+            return "left"
+        elif check("wall", x + 1, y) != 1:
+            return "right"
+        elif check("wall", x , y - 1) != 1:
+            return "up"
+    elif check('level') == 3:
+        if check("gold", x, y):
+            return "take"
+        if check("wall", x, y + 1) == 1 and check("wall", x - 1, y) != 1:
+            return "left"
+        elif check("wall", x - 1, y) == 1 and check("wall", x, y - 1) != 1:
+            return "up"
+        elif check("wall", x , y - 1) == 1 and check("wall", x + 1, y) != 1:
+            return "right";
+        elif check("wall", x + 1 , y) == 1 and check("wall", x, y + 1) != 1:
+            return "down";
+        if check("wall", x - 1, y - 1) == 1:
+            return "up"
+        elif check("wall", x + 1, y - 1) == 1:
+            return "right"
+        elif check("wall", x + 1, y + 1) == 1:
+            return "down"
+        elif check("wall", x - 1, y + 1) == 1:
+            return "left"
+    elif check('level') == 4:
+        if check("gold", x, y):
+            return "take"
+        if check("wall", x - 1, y) == 1 and check("wall", x + 2, y) == 1 and (check("wall", x - 2, y) != 1):
+            return "right"
+        elif check("wall", x + 1, y + 1) == 1 and check("wall", x - 1, y) != 1 and (check("wall", x - 2, y) == 1) and check("wall", x + 1, y) != 1:
+            return "left"
+        elif check("wall", x, y + 1) == 1 and check("wall", x - 1, y) != 1:
+            return "left"
+        elif check("wall", x - 1, y) == 1 and check("wall", x, y - 1) != 1:
+            return "up"
+        elif check("wall", x , y - 1) == 1 and check("wall", x + 1, y) != 1:
+            return "right";
+        elif check("wall", x + 1 , y) == 1 and check("wall", x, y + 1) != 1:
+            return "down";
+        if check("wall", x - 1, y - 1) == 1:
+            return "up"
+        elif check("wall", x + 1, y - 1) == 1:
+            return "right"
+        elif check("wall", x + 1, y + 1) == 1:
+            return "down"
+        elif check("wall", x - 1, y + 1) == 1:
+            return "left"
+
     return "pass"
